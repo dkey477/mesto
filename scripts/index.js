7c96a1a9899383522c13398f433d51bee5306309
dkey <dkeykireev@yandex.ru>
2022-12-28 22:29:05 +0300

    fix: иправить замечание ревью 1

--- scripts/index.js
+++ scripts/index.js
@@ -3,23 +3,22 @@ const popupElement = document.querySelector('.popup');
 const popupContElement = popupElement.querySelector('.popup__content');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupOpenButtonElement = document.querySelector('.profile__popup-open');
-const popupSaveButtonElement = popupElement.querySelector('.popup__save');
 //Перенос текста из Профиля в попап
 const profileElement = document.querySelector('.profile');
 let profileTitle = profileElement.querySelector('.profile__title');
 let profileSubtitle = profileElement.querySelector('.profile__subtitle');
-let popupName = popupElement.querySelector('.popup__name');
-let popupAbout = popupElement.querySelector('.popup__about');
+let popupName = popupElement.querySelector('.popup__input_type_name');
+let popupAbout = popupElement.querySelector('.popup__input_type_about');
 
 
 // Включение выключение попапа
 const openPopup = function() {
-  popupElement.classList.add('popup__open');
+  popupElement.classList.add('popup_open');
   popupName.value = profileTitle.textContent;
   popupAbout.value = profileSubtitle.textContent;
 }
 const closePopup = function() {
-  popupElement.classList.remove ('popup__open');
+  popupElement.classList.remove ('popup_open');
 }
 
 popupOpenButtonElement.addEventListener('click', openPopup);
@@ -28,8 +27,8 @@ popupCloseButtonElement.addEventListener('click', closePopup);
 function insertText (evt) {
   evt.preventDefault()
   profileTitle.textContent = popupName.value;
-  profileSubtitle.textContent = popupAbout.value
+  profileSubtitle.textContent = popupAbout.value;
+  closePopup();
 }
 
 popupContElement.addEventListener('submit', insertText);
-popupSaveButtonElement.addEventListener('click', closePopup);
